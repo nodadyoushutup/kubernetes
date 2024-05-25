a8f617e7bb0334c043f57b476fb1444031eaeb5e
nodadyoushutup <admin@nodadyoushutup.com>
2024-05-25 17:57:49 +0000

    [FIX] ArgoCD

---
+++ init_argocd.sh
@@ -0,0 +1,4 @@
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode && echo
+kubectl apply -f application/argocd-application.yaml
