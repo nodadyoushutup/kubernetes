00aa4315b905b059869c0725711586de4867cdee
nodadyoushutup <admin@nodadyoushutup.com>
2024-06-01 22:01:40 +0000

    [WIP] Prowlarr

--- script/tarshot.sh
+++ script/tarshot.sh
@@ -68,9 +68,9 @@ apply_asset() {
 
 apply_assets() {
     log "Starting backup job"
-    apply_asset "volumesnapshot" "${NAMESPACE}-volumesnapshot-tarshot" "readyToUse" "true" "/git/backup/volumesnapshot-tarshot.yaml"
-    apply_asset "pvc" "${NAMESPACE}-pvc-tarshot" "phase" "Bound" "/git/backup/pvc-tarshot.yaml"
-    apply_asset "job" "${NAMESPACE}-job-tarshot" "succeeded" "1" "/git/backup/job-tarshot.yaml"
+    apply_asset "volumesnapshot" "${NAMESPACE}-volumesnapshot-tarshot" "readyToUse" "true" "/tmp/git/backup/volumesnapshot-tarshot.yaml"
+    apply_asset "pvc" "${NAMESPACE}-pvc-tarshot" "phase" "Bound" "/tmp/git/backup/pvc-tarshot.yaml"
+    apply_asset "job" "${NAMESPACE}-job-tarshot" "succeeded" "1" "/tmp/git/backup/job-tarshot.yaml"
     log "Backup job completed"
 }
 
