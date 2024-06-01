955b0cd85bf629416e24bc479a0a2dc48867d660
nodadyoushutup <admin@nodadyoushutup.com>
2024-06-01 21:31:36 +0000

    [WIP] Prowlarr

--- script/tarshot-copy.sh
+++ script/tarshot-copy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+# script/tarshot-copy.sh
 echo "Starting job at $(date)"
 FILE="/tarshot/${NAMESPACE}-backup-$(date +%Y-%m-%d-%H-%M-%S).tar.gz"
-tar -czvf ${FILE} /config
-chown 568:568 ${FILE}
+tar -czvf ${FILE} /config --owner=568 --group=568
 echo "Job completed at $(date)"
