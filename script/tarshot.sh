50be1b2df5ae0a2d5ee6c9c4b30218929a11fb3f
nodadyoushutup <admin@nodadyoushutup.com>
2024-06-01 20:32:21 +0000

    [WIP] Prowlarr

--- script/tarshot.sh
+++ script/tarshot.sh
@@ -81,5 +81,5 @@ apply_assets() {
 echo "Starting job at $(date)"
 BACKUP_FILE="/backup/${NAMESPACE}_backup_$(date +%Y_%m_%d_%H_%M_%S).tar.gz"
 tar -czvf ${BACKUP_FILE} /config
-chown 568:568 ${BACKUP_FILE}
+# chown 568:568 ${BACKUP_FILE}
 echo "Job completed at $(date)"
