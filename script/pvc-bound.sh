e2292c886f7e8dc799d43cec22d436984c028933
nodadyoushutup <admin@nodadyoushutup.com>
2024-06-01 18:43:24 +0000

    [WIP] Prowlarr

---
+++ script/pvc-bound.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Waiting for PVC to be bound in ${NAMESPACE} namespace..."
+until [ "$(kubectl get pvc ${NAMESPACE}-pvc -n ${NAMESPACE} -o jsonpath='{.status.phase}')" = "Bound" ]; do
+    echo "PVC not bound yet. Waiting..."
+    sleep 2
+done
+echo "PVC is bound."
