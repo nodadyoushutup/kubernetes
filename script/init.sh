32e1f31ed866c3f0cd7e1b21d4493b2f39990915
nodadyoushutup <admin@nodadyoushutup.com>
2024-06-02 00:15:23 +0000

    [WIP] Prowlarr

--- script/init.sh
+++ script/init.sh
@@ -8,15 +8,15 @@ if [ -z "$NAMESPACE" ]; then
 fi
 
 # Define the tar.gz file path
-TAR_FILE="/init/${NAMESPACE}_init.tar.gz"
+FILE="/init/${NAMESPACE}_init.tar.gz"
 
 # Check if the tar.gz file exists
-if [ -f "$TAR_FILE" ]; then
-  echo "Found $TAR_FILE. Extracting to /config..."
+if [ -f "$FILE" ]; then
+  echo "Found $FILE. Extracting to /config..."
 
   # Extract the contents to /config and overwrite any existing files
-  tar -xzvf "$TAR_FILE" -C /config --overwrite
+  tar -xzvf "$FILE" -C /config --overwrite
   echo "Extraction complete."
 else
-  echo "File $TAR_FILE does not exist. No migration action is necessary."
+  echo "File $FILE does not exist. No migration action is necessary."
 fi
