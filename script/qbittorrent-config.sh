6f5762b966ed6c045e01d8c9a3258f04d47393fc
nodadyoushutup <admin@nodadyoushutup.com>
2024-06-22 17:58:32 +0000

    [WIP] qBittorrent

--- script/qbittorrent-config.sh
+++ script/qbittorrent-config.sh
@@ -10,30 +10,28 @@ add_config_to_section() {
     local value="$2"
     local section="$3"
     local file="$4"
+    local tempfile=$(mktemp)
 
     echo "Checking if '$key' exists in the [$section] section of $file."
 
-    # Check if the key already exists in the specified section
-    if ! grep -q "^$key=$value" "$file"; then
-        echo "Adding '$key=$value' to the [$section] section in $file."
+    awk -v key="$key" -v value="$value" -v section="$section" '
+    BEGIN { in_section = 0; key_found = 0; }
+    /^\[.*\]/ { in_section = 0; }
+    /^\['"$section"'\]/ { in_section = 1; }
+    in_section && $0 ~ "^" key "=" { key_found = 1; $0 = key "=" value; }
+    { print; }
+    END {
+        if (!key_found) {
+            if (!in_section) {
+                print "[" section "]";
+            }
+            print key "=" value;
+        }
+    }
+    ' "$file" > "$tempfile"
 
-        # Find the line number where the section starts
-        local section_line=$(grep -n "^\[$section\]" "$file" | cut -d ':' -f 1)
-
-        if [ -n "$section_line" ]; then
-            # Add the setting right after the section header
-            section_line=$((section_line + 1))
-            sed -i "${section_line}i$key=$value" "$file"
-        else
-            # If the section is not found, append the section and setting at the end of the file
-            echo "[$section]" >> "$file"
-            echo "$key=$value" >> "$file"
-        fi
-
-        echo "'$key=$value' added to the [$section] section in the configuration."
-    else
-        echo "'$key=$value' is already present in the [$section] section."
-    fi
+    mv "$tempfile" "$file"
+    echo "'$key=$value' added to the [$section] section in the configuration."
 }
 
 # Check if the destination configuration file exists
@@ -47,7 +45,7 @@ if [ ! -f "$CONF_FILE" ]; then
     # Copy the default configuration file to the destination
     cp "$DEFAULT_FILE" "$CONF_FILE"
 
-    add_config_to_section "Session\\Port" $PORT_TRAFFIC "BitTorrent" "$CONF_FILE"
+    add_config_to_section "Session\\Port" "$PORT_TRAFFIC" "BitTorrent" "$CONF_FILE"
 
     echo "Default configuration copied."
 else
